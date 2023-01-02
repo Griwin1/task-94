d98a44c99369785b52c0fd0e2bf62b2154f73e5f
Nikola Orlovski <nikikiko1@abv.bg>
2023-01-02 18:31:03 +0200

    Javascript Application logic

--- src/js/Application.js
+++ src/js/Application.js
@@ -14,8 +14,25 @@ export default class Application extends EventEmitter {
     this.emit(Application.events.READY);
   }
   setEmojis(emojis) {
+    // update the emojis array
     this.emojis = emojis;
+
+    // get the emojis wrapper div from the DOM (id="emojis" in HTML)
+    const emojisWrapperDiv = document.getElementById("emojis");
+
+    // create new paragraph
+    const para = document.createElement("p");
+    // set the text content of the new paragraph to be equal to the new emojis array (parameter)
+    para.textContent = this.emojis;
+
+    // add the new paragraph element to the emojis wrapper div
+    emojisWrapperDiv.replaceChildren(para);
   }
 
-  addBananas() {}
+  addBananas() {
+    const monkeysWithBananas = this.emojis.map((monkey) => {
+      return monkey + this.banana;
+    })
+    this.setEmojis(monkeysWithBananas)
+  }
 }
